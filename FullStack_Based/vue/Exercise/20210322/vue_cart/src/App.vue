18e379f05e53c05b8cab273c056501c3a6fe2292
L-cuitain <948527099@qq.com>
2021-04-09 07:50:48 +0800

    update:vite框架搭建

---
+++ FullStack_Based/vue/Exercise/20210322/vue_cart/src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <a-layout class="home">
+    <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
+      <div class="logo" />
+      <a-menu theme="dark" mode="inline" v-model:selectedKeys="selectedKeys">
+        <a-menu-item key="1">
+          
+          <router-link to="/about"><user-outlined />关于</router-link>
+        </a-menu-item>
+        <a-menu-item key="2">
+          
+          <router-link to="/cart"><video-camera-outlined />购物</router-link>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <menu-unfold-outlined
+          v-if="collapsed"
+          class="trigger"
+          @click="() => (collapsed = !collapsed)"
+        />
+        <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
+      </a-layout-header>
+      <a-layout-content
+        :style="{ margin: '24px 16px', padding: '24px', background: '#fff' }"
+      >
+        <router-view></router-view>
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
+</template>
+<script>
+import { defineComponent , ref} from 'vue'
+
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+} from '@ant-design/icons-vue';
+
+export default defineComponent({
+  components: {
+    UserOutlined,
+    VideoCameraOutlined,
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+  },
+  setup() {
+    return{
+      selectedKeys: ref(['1']),
+      collapsed: ref(false),
+    }
+  },
+})
+</script>
+
+<style>
+
+#app{
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  height: 100%;
+}
+
+.home{
+  height: 100%;
+}
+
+.trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+.trigger:hover {
+  color: #1890ff;
+}
+
+.logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.3);
+  margin: 16px;
+}
+
+.site-layout .site-layout-background {
+  background: #fff;
+}
+</style>
---
+++ FullStack_Based/vue/Exercise/20210322/vue_cart/src/views/Cart.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="my_echarts"></div>
+</template>
+
+<script>
+import { onMounted , reactive } from "vue";
+import * as echarts from "echarts";
+
+export default {
+  setup() {
+    const options = reactive({
+      title: {
+        text: "ECharts 入门示例",
+      },
+      tooltip: {},
+      legend: {
+        data: ["销量"],
+      },
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "销量",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    });
+
+    onMounted(() => {
+        //初始化echarts
+        const mychart = echarts.init(document.getElementById("my_echarts"));
+
+        //使用刚指定的配置和数据显示图表
+        mychart.setOption(options);
+    });
+  },
+};
+</script>
+
+<style>
+#my_echarts {
+  width: 600px;
+  height: 400px;
+}
+</style>
